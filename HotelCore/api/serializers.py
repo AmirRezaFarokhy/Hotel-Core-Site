ec5cfdf6d4a2a01d21d6b0b1ae9a92eaf5ba9ebe
AmirRezaFarokhy <amirrreza.farokhy81@gmail.com>
2024-06-04 15:40:07 +0330

    Convert Miladi to Shamsi datetime

--- HotelCore/api/serializers.py
+++ HotelCore/api/serializers.py
@@ -1,6 +1,6 @@
 from HotelService.models import Hotel, City
 from rest_framework import serializers
- 
+from base.mixin import BaseDateMixin
 
 class LocationSerializer(serializers.ModelSerializer):
     
@@ -8,9 +8,22 @@ class LocationSerializer(serializers.ModelSerializer):
         model = City
         fields = '__all__'
 
-class HotelSerializer(serializers.ModelSerializer):
+
+class HotelSerializer(serializers.ModelSerializer, BaseDateMixin):
 
     location = serializers.SerializerMethodField()
+    created_at = serializers.SerializerMethodField()
+    updated_at = serializers.SerializerMethodField()
+    deleted_at = serializers.SerializerMethodField()
+
+    def get_created_at(self, obj):
+        return self.convert_date(obj.created_at)
+    
+    def get_updated_at(self, obj):
+        return self.convert_date(obj.updated_at)
+    
+    def get_deleted_at(self, obj):
+        return self.convert_date(obj.deleted_at)
 
     def get_location(self, obj):
         return obj.location.address
